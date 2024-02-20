3e5e53963456ab1809752f34a5a16b9ff0cb6bc3
luidimso <luidi_mso@outlook.com>
2024-02-20 14:36:19 -0300

    Adding swiper JS on home layout

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -12,6 +12,7 @@ import { AlertController } from '@ionic/angular';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage implements OnInit {
+
   public userId:string = "Qc8chmCKIo0KPkzZDDeU";
 
   private users: Observable<User> = this.userService.getUserById(this.userId);
@@ -20,6 +21,7 @@ export class HomePage implements OnInit {
   public hasNewProgram:boolean = false;
 
   public user: User;
+  public programsCards: Program[];
   public background: string = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRCfbdjhX8vUFbc-r7s-6vswI3Zpuz1PzcTug&usqp=CAU";
   
   constructor(
@@ -28,12 +30,25 @@ export class HomePage implements OnInit {
     private alertCtrl: AlertController
   ) {}
 
+
+
   ngOnInit(): void {
     this.users.subscribe(u => {
       this.user = u;
     });
 
     this.programs.subscribe(ps => {
+
+      this.programsCards = ps.sort((a, b) => {
+        if(new Date(a.createdAt) > new Date(b.createdAt)) {
+          return -1;
+        } else if(new Date(b.createdAt) > new Date(a.createdAt)) {
+          return 1;
+        } else {
+          return 0;
+        }
+      });
+
       this.hasNewProgram = false;
       const today = new Date();
       const sevenDaysAgo = new Date(today.getFullYear(), today.getMonth(), today.getDate() - 7);
